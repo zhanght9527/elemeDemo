4bcaef2d86047980c74a7046d5bd0ce004a9a0a8
electricUnion <532617457@qq.com>
2017-11-27 17:44:25 +0800

    11.27

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -25,7 +25,35 @@
             <transition name="fade">
               <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
             </transition>
+          </div>
+          <split v-show="food.info"></split>
+          <div class="info" v-show="food.info">
+            <h1 class="title">商品信息</h1>
+            <p class="text">{{food.info}}</p>
+          </div>
+          <split></split>
+          <div class="rating">
+            <h1 class="title">商品评价</h1>
+            <ratingselect :select-type='selectType' :only-content='onlyContent' :desc='desc' :ratings='food.ratings'></ratingselect>
+            <div class="rating-wrapper">
+              <ul v-show="food.ratings && food.ratings.length">
+                <li class="rating-item" v-for="rating in food.ratings">
+                  <div class="top-wrapper">
+                    <div class="time">{{rating.rateTime | formatDate}}</div>
+                    <div class="user">
+                      <span class="name">{{rating.username}}</span>
+                      <img class="avatar" :src="rating.avatar">
+                    </div>
+                  </div>
+                  <p class="text">
+                    <span :class="{'ele-thumb_up':rating.rateType===0,'ele-thumb_down':rating.rateType===1}"></span>
+                    {{rating.text}}
+                  </p>
+                </li>
+              </ul>
+              <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
             </div>
+          </div>
         </div>
       </div>
     </transition>
@@ -35,11 +63,26 @@
 <script>
 import BScroll from 'better-scroll'
 import cartcontral from '../cartcontrol/cartcontrol'
+import split from '../split/split'
+import ratingselect from '../ratingselect/ratingselect'
 import Vue from 'vue'
+import {formatDate} from 'common/js/date'
+
+// const POSITIVE = 0
+// const NEGATIVE = 1
+const ALL = 2
+
 export default {
   data () {
     return {
-      showFlag: false
+      showFlag: false,
+      selectType: ALL,
+      onlyContent: true,
+      desc: {
+        all: '全部',
+        positive: '推荐',
+        negative: '吐槽'
+      }
     }
   },
   props: {
@@ -50,6 +93,8 @@ export default {
   methods: {
     show () {
       this.showFlag = true
+      this.selectType = ALL
+      this.onlyContent = true
       this.$nextTick(() => {
         if (!this.scroll) {
           this.scroll = new BScroll(this.$refs.food, {
@@ -69,13 +114,22 @@ export default {
       this.$root.eventHub.$emit('cart.add', event.target)
     }
   },
+  filters: {
+    formatDate (time) {
+      let date = new Date(time)
+      return formatDate(date, 'yyyy-MM-dd  hh:mm')
+    }
+  },
   components: {
-    cartcontral
+    cartcontral,
+    split,
+    ratingselect
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+  @import "../../common/stylus/mixin.styl"
   .food
     position fixed
     top 0
@@ -163,4 +217,58 @@ export default {
         &.fade-enter,&.fade-leave-active
           opacity 0
           z-index -1
+    .info
+      padding 0.36rem
+      font-size 0
+      .title
+        font-size 14px
+        line-height 14px
+        color rgb(7,17,27)
+      .text
+        padding 0.12rem 0.16rem 0
+        font-size 12px
+        color rgb(77,85,93)
+        line-height 24px
+        text-align justify
+    .rating
+      padding-top 0.36rem
+      font-size 0
+      .title
+        line-height .28rem
+        margin-left 0.36rem
+        font-size 14px
+        color rgb(7,17,27)
+      .rating-wrapper
+        padding 0 0.36rem
+        .rating-item
+          border-1px(rgba(7,17,27,0.1))
+          padding 0.32rem 0
+          &:last-of-type
+            border:none
+          .top-wrapper
+            display flex
+            justify-content space-between
+            font-size 10px
+            line-height 12px
+            color rgb(147,153,159)
+            .user
+              font-size 0
+              .name
+                font-size 10px
+                line-height 12px
+                margin-right 0.12rem
+              .avatar
+                height 12px
+                vertical-align top
+                border-radius 50%
+          .text
+            margin-top 0.12rem
+            font-size 12px
+            line-height 16px
+            span 
+              line-height 16px
+              &.ele-thumb_up
+                color rgb(0,160,220)
+              &.ele-thumb_down
+                color rgb(147,153,159)
 </style>
---
+++ src/components/ratingselect/ratingselect.vue
@@ -0,0 +1,125 @@
+<template>
+  <div>
+    <div class="ratingselect">
+      <div class="rating-type">
+        <span class="block positive" :class="{'active':selectType===2}" @click="select(2,$event)">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
+        <span class="block positive" :class="{'active':selectType===0}" @click="select(0,$event)">{{desc.positive}}<span class="count">{{positives.length}}</span></span>
+        <span class="block negative" :class="{'active':selectType===1}" @click="select(1,$event)">{{desc.negative}}<span class="count">{{negatives.length}}</span></span>
+      </div>
+      <div class="switch" :class="{'on':onlyContent}" @click="toggleContent">
+        <span class="ele-check_circle"></span>
+        <span class="text">只看有内容的评价</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const POSITIVE = 0
+const NEGATIVE = 1
+const ALL = 2
+
+export default {
+  props: {
+    ratings: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    selectType: {
+      type: Number,
+      default: ALL
+    },
+    onlyContent: {
+      type: Boolean,
+      default: false
+    },
+    desc: {
+      type: Object,
+      default () {
+        return {
+          all: '全部',
+          positive: '满意',
+          negative: '不满意'
+        }
+      }
+    }
+  },
+  computed: {
+    positives () {
+      return this.ratings.filter((rating) => {
+        return rating.rateType === POSITIVE
+      })
+    },
+    negatives () {
+      return this.ratings.filter((rating) => {
+        return rating.rateType === NEGATIVE
+      })
+    }
+  },
+  methods: {
+    select (type, event) {
+      if (!event._constructed) {
+        return
+      }
+      this.$emit('select', type)
+    },
+    toggleContent (event) {
+      if (!event._constructed) {
+        return
+      }
+      this.$emit('toggle')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "../../common/stylus/mixin.styl"  
+  .ratingselect
+    .rating-type
+      padding 0.36rem 0
+      margin 0 0.36rem
+      border-1px(rgba(7, 17, 27, 0.1))
+      font-size 0
+      .block
+        display inline-block
+        padding 8px 12px
+        margin-right 0.16rem
+        line-height 0.32rem
+        border-radius 1px
+        font-size 12px
+        color: rgb(77, 85, 93)
+        &.active
+          color: #fff
+        .count
+          margin-left: 2px
+          font-size: 8px
+        &.positive
+          background: rgba(0, 160, 220, 0.2)
+          &.active
+            background: rgb(0, 160, 220)
+        &.negative
+          background: rgba(77, 85, 93, 0.2)
+          &.active
+            background: rgb(77, 85, 93)
+    .switch
+      padding 0.24rem 0.36rem
+      line-height 24px
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+      color: rgb(147, 153, 159)
+      font-size: 0
+      &.on
+        .icon-check_circle
+          color: #00c850
+      .ele-check_circle
+        display: inline-block
+        vertical-align: top
+        margin-right: 4px
+        font-size: 24px
+      .text
+        display: inline-block
+        vertical-align: top
+        font-size: 12px
+</style>
